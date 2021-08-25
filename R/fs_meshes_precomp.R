edf1b5e6fa8a9fbb61417ad0e0838f76fea66b7c
Tim Schäfer <ts+code@rcmd.org>
2021-08-25 11:11:00 +0200

    add functions to get precomputed mesh neighborhoods for fsaverage and fsa6

--- R/fs_meshes_precomp.R
+++ R/fs_meshes_precomp.R
@@ -2,10 +2,23 @@
 
 #' @title Return pre-computed neighborhood data for specific meshes.
 #'
-#' @param meshname a text identifier specifying the mesh you want connectivty data for. Currently supported meshes are listed here. 'lh_fsaverage': the left hemisphere of the FreeSurfer 6 fsaverage template. 'rh_fsaverage': the right hemisphere of the FreeSurfer 6 fsaverage template. 'lh_fsaverage6': the left hemisphere of the FreeSurfer 6 fsaverage6 template. 'rh_fsaverage6': the right hemisphere of the FreeSurfer 6 fsaverage6 template.
+#' @param meshname a text identifier specifying the mesh you want connectivity data for. Currently supported meshes are listed here. 'lh_fsaverage': the left hemisphere of the FreeSurfer 6 fsaverage template. 'rh_fsaverage': the right hemisphere of the FreeSurfer 6 fsaverage template. 'lh_fsaverage6': the left hemisphere of the FreeSurfer 6 fsaverage6 template. 'rh_fsaverage6': the right hemisphere of the FreeSurfer 6 fsaverage6 template.
 #'
 #' @return list of vectors, the connectivity data as an adjacency list. The outer list has length n, where n is the number of vertices in the graph. The inner lists represent, for each vertex, all of its neighbors.
 #'
 #' @export
 mesh_neigh_pre <- function(meshname) {
+  vvfile = NULL;
+  if(meshname == "lh_fsaverage") {
+    vvfile = system.file("extdata", "fsaverage_lh_white_meshdist_edge_1.vv", package = "smoothr", mustWork = TRUE);
+  } else if(meshname == "rh_fsaverage") {
+    vvfile = system.file("extdata", "fsaverage_rh_white_meshdist_edge_1.vv", package = "smoothr", mustWork = TRUE);
+  } else if(meshname == "lh_fsaverage6") {
+    vvfile = system.file("extdata", "fsaverage6_lh_white_meshdist_edge_1.vv", package = "smoothr", mustWork = TRUE);
+  } else if(meshname == "rh_fsaverage6") {
+    vvfile = system.file("extdata", "fsaverage6_rh_white_meshdist_edge_1.vv", package = "smoothr", mustWork = TRUE);
+  } else {
+    stop("Invalid mesh name. See function help for supported ones.");
+  }
+  return(read.vv(vvfile));
 }
---
+++ tests/testthat/test_fs_meshes_precomp.R
@@ -0,0 +1,27 @@
+
+test_that("The 1-ring graph neighborhood for fsaverage lh can be retrieved.", {
+  vv = mesh_neigh_pre("lh_fsaverage");
+  testthat::expect_true(is.list(vv));
+  testthat::expect_equal(length(vv), 163842L);
+})
+
+
+test_that("The 1-ring graph neighborhood for fsaverage rh can be retrieved.", {
+  vv = mesh_neigh_pre("rh_fsaverage");
+  testthat::expect_true(is.list(vv));
+  testthat::expect_equal(length(vv), 163842L);
+})
+
+
+test_that("The 1-ring graph neighborhood for fsaverage6 lh can be retrieved.", {
+  vv = mesh_neigh_pre("lh_fsaverage6");
+  testthat::expect_true(is.list(vv));
+  testthat::expect_equal(length(vv), 40962L);
+})
+
+
+test_that("The 1-ring graph neighborhood for fsaverage6 rh can be retrieved.", {
+  vv = mesh_neigh_pre("rh_fsaverage6");
+  testthat::expect_true(is.list(vv));
+  testthat::expect_equal(length(vv), 40962L);
+})
