ae3226dc7820dd0a76853a2d61b48e8b2da5637d
Tim Schäfer <ts+code@rcmd.org>
2021-10-11 14:49:46 +0200

    add parallel examples

--- web/examples/client_parallel_smooth.R
+++ web/examples/client_parallel_smooth.R
@@ -10,8 +10,9 @@ library("haze");
 
 
 mesh = freesurferformats::read.fs.surface(system.file("extdata", "fsaverage6_mesh_lh_white", package = "haze", mustWork = TRUE));
-mesh_adj = haze::mesh.adj(mesh, k = 1L); # compute 1-ring neighborhood
+mesh_adj = haze::mesh.adj(mesh, k = 1L); # Compute 1-ring neighborhood
 
+# Prepare the data.
 nv = nrow(mesh$vertices); # number of mesh vertices
 
 data1 = rnorm(nv, mean=1.0, sd=0.01); # Generate some random data.
@@ -21,9 +22,9 @@ data4 = rnorm(nv, mean=7.0, sd=0.01); # ...
 data5 = rnorm(nv, mean=9.0, sd=0.01); # ...
 data_matrix = rbind(data1, data2, data3, data4, data5); # your data as a matrix.
 
-options("mc.cores" = 5L);   # Request 5 CPU cores.
 
-# Get the smoothed matrix:
+# Compute the smoothed matrix in parallel:
+options("mc.cores" = 5L);   # Request 5 CPU cores.
 smoothed = haze::pervertexdata.smoothnn.adj(mesh_adj, data_matrix, num_iter = 15L);
 
 
