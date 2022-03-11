5b1d1dee1da638d8e1f540db51e4d2dc31616ebc
Tim Schäfer <ts+code@rcmd.org>
2022-03-11 18:00:59 +0100

    return fake data and war

--- R/kdtree.R
+++ R/kdtree.R
@@ -1,9 +1,9 @@
 
 
 
-#' @title Map input values at points in 3D space onto mesh vertices.
+#' @title Return interpolated values of the mesh data at the given query points.
 #'
-#' @description Map input values at points in 3D space onto mesh vertices. For this to make any sense, the meshes must be very similar in shape. Typically both meshes are spherical meshes with identical radius, and only the vertex positions on the sphere differ.
+#' @description Return interpolated values of the mesh data at the given query points. For this to make any sense, the meshes must be spherical with identical radius, and only the vertex positions on the sphere differ.
 #'
 #' @param input_coordinates nx3 numerical matrix of x,y,z coordinates.
 #'
@@ -13,7 +13,7 @@
 #'
 #' @return the interpolated per-vertex data for the mesh given in the \code{mesh} parameter.
 #'
-#' @seealso \code{https://github.com/ThomasYeoLab/CBIG/blob/master/external_packages/SD/SDv1.5.1-svn593/BasicTools/external_packages/SD/SDv1.5.1-svn593/BasicTools/MARS_NNInterpolate_kdTree.m}
+#' @seealso \code{https://github.com/ThomasYeoLab/CBIG/blob/master/external_packages/SD/SDv1.5.1-svn593/BasicTools/MARS_NNInterpolate_kdTree.m}
 #'
 #' @keywords internal
 # @export
