acb824cb918c2dff7b5922d21113850bb7d9758d
Tim Schäfer <ts+code@rcmd.org>
2022-03-11 17:49:34 +0100

    add skeleton for kdtree functions

--- R/kdtree.R
+++ R/kdtree.R
@@ -7,7 +7,7 @@
 #'
 #' @param input_coordinates nx3 numerical matrix of x,y,z coordinates.
 #'
-#' @param mesh an fs.surface instance, see \code{freesurferformats::read.fs.surface} to get one.
+#' @param mesh an fs.surface instance, see \code{\link[freesurferformats]{read.fs.surface}} to get one.
 #'
 #' @param input_values numerical vector, the continuous per-vertex data for the point at the input_coordinates.
 #'
@@ -25,7 +25,26 @@ nn_interpolate_kdtree <- function(input_coordinates, mesh, input_values) {
 #'
 #' @inheritParams nn_interpolate_kdtree
 #'
-#' @seealso  \code{https://github.com/ThomasYeoLab/CBIG/blob/master/external_packages/SD/SDv1.5.1-svn593/BasicTools/MARS_linearInterpolate_kdTree}
+#' @seealso  \code{https://github.com/ThomasYeoLab/CBIG/blob/master/external_packages/SD/SDv1.5.1-svn593/BasicTools/MARS_linearInterpolate_kdTree.m}
+#'
+#' @export
 linear_interpolate_kdtree <- function(input_coordinates, mesh, input_values) {
 
 }
+
+
+#' @title Find nearest mesh vertex for a coordinate using kdtree.
+#'
+#' @inheritParams nn_interpolate_kdtree
+#'
+#' @return integer vector, the \code{n} vertex indices which are closest to the \code{nx3} matrix of input_coordinates.
+#'
+#' @seealso  \code{https://github.com/ThomasYeoLab/CBIG/blob/master/external_packages/SD/SDv1.5.1-svn593/BasicTools/MARS_findNV_kdTree.m}
+#'
+#' @export
+find_nv_kdtree <- function(input_coordinates, mesh) {
+
+}
+
+
+
