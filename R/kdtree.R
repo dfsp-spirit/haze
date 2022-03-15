8fec32cb35a74cfbe457d3041fcf10dc8218d0f3
Tim Schäfer <ts+code@rcmd.org>
2022-03-15 20:34:03 +0100

    work on finding nearest verts with kdtree

--- R/kdtree.R
+++ R/kdtree.R
@@ -46,10 +46,34 @@ linear_interpolate_kdtree <- function(input_coordinates, mesh, input_values) {
 #' @seealso  \code{https://github.com/ThomasYeoLab/CBIG/blob/master/external_packages/SD/SDv1.5.1-svn593/BasicTools/MARS_findNV_kdTree.m}
 #'
 #' @keywords internal
+#'
+#' @importFrom parallel detectCores
+#' @importFrom Rvcg vcgCreateKDtreeFromBarycenters vcgClostOnKDtreeFromBarycenters
 # export
 find_nv_kdtree <- function(input_coordinates, mesh) {
   warning("find_nv_kdtree: NOT IMPLEMENTED YET, returning fake data.");
   return(seq.int(nrow(input_coordinates)));
+
+  mesh = ensure.fs.surface(mesh);
+  if(! freesurferformats::is.fs.surface(mesh)) {
+    stop("Parameter 'mesh' must be an fs.surface instance.");
+  }
+  if(is.matrix(input_coordinates)) {
+    if(ncol(input_coordinates) != 3L) {
+      stop("The matrix 'input_coordinates' must have exactly 3 columns.");
+    }
+    if(! is.numeric(input_coordinates)) {
+      stop("The matrix 'input_coordinates' must be numeric.");
+    }
+    tmesh = ensure.tmesh3d(mesh);
+    kdtreeBary = Rvcg::vcgCreateKDtreeFromBarycenters(tmesh);
+    #nv_mesh = Rvcg::vcgClostOnKDtreeFromBarycenters(kdtreeBary, input_coordinates, threads=parallel::detectCores())
+    #closest_vertex_coords = nv_mesh$vb;
+    #closest_vertex_distances = nv_mesh$quality;
+    res = Rvcg::vcgSearchKDtree(kdtreeBary, input_coordinates, k=1L, threads = parallel::detectCores());
+  } else {
+    stop("Parameter 'input_coordinates' must be a matrix.");
+  }
 }
 
 
